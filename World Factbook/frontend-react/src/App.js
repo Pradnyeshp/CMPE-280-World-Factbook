705936c485ec51984194208a657463982d4896ab
Pradnyesh <pradnyesh.patil@sjsu.edu>
2019-02-21 14:47:39 -0800

    Minor updates
    Minor updates

--- World Factbook/frontend-react/src/App.js
+++ World Factbook/frontend-react/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import Home from './components/Home';
+import Population from './components/Population';
+import Area from "./components/Area";
 
 class App extends Component {
   render() {
     return (
         <Switch>
             <Route exact path = '/' component = {Home}/>
+            <Route exact path = '/population' component = {Population}/>
+            <Route exact path = '/area' component = {Area}/>
         </Switch>
     );
   }
--- World Factbook/frontend-react/src/components/Home.js
+++ World Factbook/frontend-react/src/components/Home.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import Navbar from "./Navbar";
+// import {Link} from "react-router-dom";
+import '../css/CardStyle.css';
+import Categories from "./Categories";
 
 class Home extends Component {
 
@@ -13,52 +16,7 @@ class Home extends Component {
             <div>
                 <Navbar/>
                 <br/>
-                <div className="container small">
-                    <h1>Categories</h1>
-                    <br/>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: "21rem", float : 'left', marginRight : '1.1em', marginBottom : '1.3em', height : '12rem'}}>
-                        <div className="card-body">
-                            <h5 className="card-title">People and Society</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: "21rem", float : 'left', marginRight : '1.1em', height : '12rem'}}>
-                        <div className="card-body">
-                            <h5 className="card-title">Geography</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: "21rem", float : 'left', marginRight : '1.1em', height : '12rem'}}>
-                        <div className="card-body">
-                            <h5 className="card-title">Economy</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: "21rem", float : 'left', marginRight : '1.1em', height : '12rem'}}>
-                        <div className="card-body">
-                            <h5 className="card-title">Energy</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: "21rem", float : 'left', marginRight : '1.1em', height : '12rem'}}>
-                        <div className="card-body">
-                            <h5 className="card-title">Transpotation</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                        </div>
-                    </div>
-                    <div className="card text-white bg-dark mb-3" style={{maxWidth: "21rem", float : 'left', marginRight : '1.1em', height : '12rem'}}>
-                        <div className="card-body">
-                            <h5 className="card-title">Communications</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the
-                                bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
+                <Categories/>
                 <br/>
                 <br/>
                 <br/>
