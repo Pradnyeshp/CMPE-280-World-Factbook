f002d32589cbe010202e5cb260aa94f482355937
Pradnyesh <pradnyesh.patil@sjsu.edu>
2019-04-19 19:07:49 -0700

    Added Geograph
    Added Geograph

--- World Factbook/frontend-react/src/App.js
+++ World Factbook/frontend-react/src/App.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import Home from './components/Home';
-import PopulationGraph from './components/PopulationGraph';
+import PopulationGrowthGraph from './components/PopulationGrowthGraph';
 import Area from "./components/Area";
 import CountryDetails from './components/CountryDetails';
 import CountryList from './components/CountryList';
 import Form from './components/Form';
 import HomePage from './components/HomePage';
 import Profile from "./components/Profile";
+import GeoGraph from "./components/GeoGraph";
 
 class App extends Component {
   render() {
     return (
         <Switch>
             <Route exact path = '/' component = {HomePage}/>
-            <Route exact path = '/population' component = {PopulationGraph}/>
+            <Route exact path = '/population' component = {PopulationGrowthGraph}/>
             <Route exact path = '/area' component = {Area}/>
             <Route path = '/getcountry/:country' component = {CountryDetails} />
             <Route path = '/countrylist' component = {CountryList} />
             <Route exact path = '/dashboard/:country' component = {Home} />
             <Route exact path = '/Form' component = {Form} />
             <Route exact path = '/userprofile' component = {Profile} />
-
+            <Route exact path = '/geograph' component = {GeoGraph} />
         </Switch>
     );
   }
---
+++ World Factbook/frontend-react/src/components/GeoGraph.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import Navbar from "./Navbar";
+import axios from 'axios';
+import { Chart } from "react-google-charts";
+
+class GeoGraph extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            country : "india" ,
+            populationCountArray : []
+        }
+    }
+
+    componentWillMount() {
+
+        let url = `http://localhost:3001/populationCount/${this.state.country}` ;
+        axios.get(url)
+            .then(response=>{
+                console.log(response.data) ;
+                this.setState({
+                    populationCountArray : response.data.data
+                })
+            })
+    }
+
+    render() {
+
+        // console.log(this.state.populationCountArray) ;
+        let populationCountArray = this.state.populationCountArray ;
+        let currentPopulation ;
+
+        for(let i = 0 ; i < populationCountArray.length ; i++){
+
+            let row = populationCountArray[i] ;
+            if(row.year === "2019"){
+                currentPopulation = row.value ;
+                break;
+            }
+        }
+
+        console.log(currentPopulation);
+
+
+        return(
+            <div>
+                <Navbar/>
+                <br/>
+                <h2>
+                    Geo Graph
+                </h2>
+                Geo Chart
+                <Chart
+                    width={'1200px'}
+                    height={'700px'}
+                    chartType="GeoChart"
+                    data={[
+                        ['Country', 'Population'],
+                        [this.state.country.toLocaleUpperCase(), currentPopulation],
+                        // ['United States', 300],
+                        // ['Brazil', 400],
+                        // ['Canada', 500],
+                        // ['France', 600],
+                        // ['RU', 700],
+                    ]}
+                    // Note: you will need to get a mapsApiKey for your project.
+                    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+                    mapsApiKey="AIzaSyBgT9S1jtBpZ7HNyqTz86ay1uEeHVj0bMY"
+                    rootProps={{ 'data-testid': '1' }}
+                />
+            </div>
+        )
+    }
+
+}
+
+export default GeoGraph;
